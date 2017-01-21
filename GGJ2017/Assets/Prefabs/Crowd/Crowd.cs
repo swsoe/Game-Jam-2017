4b25d0f71cef033c401d3258f8c4632e57d8ff46
swsoe <npt1@zips.uakron.edu>
2017-01-21 13:17:42 -0500

    Added character type functionality plus framework for logic to be fired on collision

--- GGJ2017/Assets/Prefabs/Crowd/Crowd.cs
+++ GGJ2017/Assets/Prefabs/Crowd/Crowd.cs
@@ -8,14 +8,30 @@ public class Crowd : MonoBehaviour {
 
     public List<UnityEngine.Material> textures;
 
+    public InputManager input = InputManager.Instance;
+
 	// Use this for initialization
 	void Start ()
     {
-        gameObject.GetComponent<UnityEngine.MeshRenderer>().material = null;
+        gameObject.GetComponent<UnityEngine.MeshRenderer>().material = textures[Random.Range(0, textures.Count)];
+        characterType = Random.Range(0, 7);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void Collided()
+    {
+        int result = input.CheckCharacter(characterType);
+        if (result > 0)
+        {
+            //trigger positive reaction
+        }
+        if (result < 0)
+        {
+            //trigger negative reaction
+        }
+    }
 }
