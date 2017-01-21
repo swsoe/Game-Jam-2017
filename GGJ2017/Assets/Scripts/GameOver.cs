8da7f47cc2a3fca1175337b0a953021a354d5b6b
swsoe <npt1@zips.uakron.edu>
2017-01-21 03:21:11 -0500

    Title Screen framework plus game over script

---
+++ GGJ2017/Assets/Scripts/GameOver.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+    public UnityEngine.AudioSource gameOverSoundSource;
+    public UnityEngine.AudioClip gameOverSound;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    public IEnumerator TriggerGameOver()
+    {
+        //Play game over sound
+        if (!gameOverSoundSource.isPlaying)
+            gameOverSoundSource.PlayOneShot(gameOverSound);
+
+        //Wait for sound to finish
+        yield return new WaitForSeconds(gameOverSound.length);
+
+        //Transition to game over screen
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameOverScreen");
+    }
+}
---
+++ GGJ2017/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+
+    private Level selectedLevel = Level.FirstAppearence;
+    public UnityEngine.UI.Text textBox;
+
+    // Use this for initialization
+    void Start ()
+    {
+        textBox.text = selectedLevel.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Next()
+    {
+        selectedLevel += 1;
+        if ((int)selectedLevel > 2)
+            selectedLevel = (Level)0;
+        textBox.text = selectedLevel.ToString();
+    }
+
+    public void Previous()
+    {
+        selectedLevel -= 1;
+        if ((int)selectedLevel < 0)
+            selectedLevel = (Level)2;
+        textBox.text = selectedLevel.ToString();
+    }
+
+    public void StartLevel()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(selectedLevel.ToString());
+    }
+
+    private enum Level {FirstAppearence, TownHall, TheBigSpeech}
+
+}
