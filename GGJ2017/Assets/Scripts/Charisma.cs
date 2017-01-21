f72d3a4163b738055e685c230b1da42ae17e7844
swsoe <npt1@zips.uakron.edu>
2017-01-20 23:09:20 -0500

    Charisma bar

---
+++ GGJ2017/Assets/Scripts/Charisma.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Charisma : MonoBehaviour {
+
+    //Starting charisma
+    public int charisma = 500;
+
+    //Charisma max
+    public int charismaMax = 1000;
+
+    //Charisma drain amount
+    public const int charismaDrain = 5;
+
+    //Charisma drain tick length in seconds
+    public float tickLength = 0.2F;
+
+    public float time;
+    public float currentTime;
+
+    //Enables/Disables the draining of 
+    public bool drain = false;
+
+    public float Percentage;
+
+    //The bar object
+    private UnityEngine.UI.Slider scrollBar;
+
+	// Use this for initialization
+	void Start ()
+    {
+        scrollBar = gameObject.GetComponent<UnityEngine.UI.Slider>();
+        scrollBar.minValue = 0;
+        scrollBar.maxValue = charismaMax;
+        scrollBar.value = charisma;
+        print(gameObject.name);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (drain)
+        {
+            currentTime = Time.time;
+            if (currentTime - time >= tickLength)
+            {
+                charisma = charisma - charismaDrain;
+                if (charisma < 0)
+                {
+                    charisma = 0;
+                    drain = false;
+                }
+                time = Time.time;
+            }
+            Percentage = (float)charisma / (float)charismaMax;
+
+            scrollBar.value = charisma;
+
+        }
+        
+	}
+
+    public void startBar()
+    {
+        drain = true;
+        time = Time.time;
+    }
+
+    public void addPoints(int points)
+    {
+        charisma += points;
+    }
+}
