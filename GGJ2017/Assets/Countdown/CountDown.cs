f184a8d58a2d434bf3178ddefe73684101a051bf
makerguide <ben@benjones.us>
2017-01-21 15:12:13 -0500

    started countdown

---
+++ GGJ2017/Assets/Countdown/CountDown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour {
+
+	public float timeLength = 60f;
+	Text timeText;
+
+	// Use this for initialization
+	void Start () {
+		timeText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void UpdateTimer(){
+		if(timeLength > 0){
+			//timeLength -= 
+		}
+	}
+}
